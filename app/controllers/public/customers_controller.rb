687ddb5981121c424883d9807dc5dac0e950928e
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-05-18 17:50:46 +0000

    create customer edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,14 +5,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to public_customers_my_page_path(@customer), notice: "更新に成功しました."
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 end
