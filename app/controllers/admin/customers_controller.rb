d377197f97edaaad723168905d2c5eb467c9605d
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-05-19 04:50:11 +0000

    sayaka

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -13,11 +13,8 @@ class Admin::CustomersController < ApplicationController
 
   def update
    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to admin_customer_path(@customer)
-    else
-      render :edit
-    end
+   @customer.update(customer_params)
+   redirect_to admin_customers_path(customer)
   end
 
   private
