e1516065e2f7e94b199d872e8877eeaf5abe58e5
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-05-19 09:25:09 +0000

    sayaka

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,6 +16,7 @@ class Admin::CustomersController < ApplicationController
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer), notice: '会員情報が更新されました。'
     else
+      #redirect_to edit_admin_customer_path(@customer)
       render :edit
     end
   end
