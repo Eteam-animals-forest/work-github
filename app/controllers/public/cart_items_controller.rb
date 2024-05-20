8fd3196102a83a42d3ad326a0c03327977c37593
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-05-20 04:32:25 +0000

    cart-item途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,12 +2,18 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   
   def index
+    @cart_items = current_customer.cart_items.includes(:item)
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+  if @cart_item.update(cart_item_params)
+     redirect_to  public_cart_items_path, notice: 'カートアイテムを更新しました'
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    
   end
 
   def create
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,5 @@ class CartItem < ApplicationRecord
   belongs_to :item, dependent: :destroy
   belongs_to :customer, dependent: :destroy
   
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
