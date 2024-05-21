db053d048e8440aa84470ca83974f78c048e49a8
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-05-21 05:08:34 +0000

    admin/homes/top動作未確認

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order, dependent: :destroy
   belongs_to :item, dependent: :destroy
+  
+  def amount_total
+    Orderdetail.sum(:amount)
+  end 
 end
